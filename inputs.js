b739f37cac68f20bc64e6a9a1ffb7f743c2ac698
nicoducceschi <nicoducceschi@hotmail.com>
2024-07-23 14:55:24 +0100

    Update inputs.js

--- inputs.js
+++ inputs.js
@@ -2,7 +2,7 @@
 Step 7: Connecting EmailOctopus and Calendly to your website
 - Add your EmailOctopus and Calendly links between the quotation marks '' below
 */
-const emailOctopusScript = '<script async src="https://eocampaign1.com/form/dc180a9e-4040-11ef-be95-d31e7313d676.js" data-form="dc180a9e-4040-11ef-be95-d31e7313d676"></script>'
+const emailOctopusScript = '<script async src="https://eocampaign1.com/form/ebd13b22-48f1-11ef-85cd-e922f42b7581.js" data-form="ebd13b22-48f1-11ef-85cd-e922f42b7581"></script>'
 const calendlyLink = 'https://calendly.com'
 
 /*
